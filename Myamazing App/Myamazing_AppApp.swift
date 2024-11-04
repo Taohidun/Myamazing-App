fdeb96d978a6bda1b922d4f191f0f54373a5b6ae
Mohammad Taohidun Nur Islam <mislam24@fed.idserve.net>
2024-11-04 15:21:23 +0100

    Initial Commit

---
+++ Myamazing App/Myamazing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myamazing_AppApp.swift
+//  Myamazing App
+//
+//  Created by Mohammad Islam on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Myamazing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
